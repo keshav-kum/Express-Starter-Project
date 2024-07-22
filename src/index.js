9afc8d16c2d1410d29bc9c58a672438882adc581
keshav kumar <keshav20102001@gmail.com>
2024-07-22 11:43:24 -0500

    Initial code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const serverConfig = require("./config/serverConfig")
+
+
+const app = express();
+
+
+
+
+
+
+
+
+app.listen(serverConfig.PORT, ()=>{
+   console.log(`server started at port ${serverConfig.PORT}`);
+})
